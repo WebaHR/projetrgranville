91460dbb9e8e2212e4066ae4b580245f3da6ddcf
WebaHR <webmasterhautrhin@gmail.com>
2026-09-10 08:47:25 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,7 @@ const identities = [
     id: 3,
     photo:    '/images/Esquisse-1_dessin_ProjetZatrap.jpg',
     photoAlt: 'ZATRAP',
-    name:     <strong>'ZATRAP'</strong>,
+    name:     <p style="font-weight:700;font-size:25px">'ZATRAP'</p>,
     role:     `ZATRAP.FR`,
     detail1:  'Revue Formes & Silences',
     detail2:  'Berlin — Paris',
